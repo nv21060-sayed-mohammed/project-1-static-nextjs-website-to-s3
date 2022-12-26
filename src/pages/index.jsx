abe305f56955d1888b4f445a876d9da78d7c19ed
nv21060-sayed-mohammed <118057466+nv21060-sayed-mohammed@users.noreply.github.com>
2022-12-26 11:05:19 +0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Sayed Mohammed - Cloud architect
+        NV21060 - Sayed Mohammed
         </title>
         <meta
           name="description"
@@ -261,11 +261,11 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Cloud Architect
+          Solution Architect
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
+            I’m Sayed Mohammed, a solution architect and entrepreneur based in
+            Bahrain. I’m the founder and CEO of NVTC, where we develop
             technologies that empower regular people to explore space on their
             own terms.
           </p>
